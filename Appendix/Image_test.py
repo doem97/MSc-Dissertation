92ce9429897cbede62471d58016f4ff948ff2379
Zichen TIAN <doem1997@gmail.com>
2021-03-25 23:21:27 +0800

    finish the chapter 2

---
+++ Appendix/Image_test.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+from tqdm import tqdm
+from scipy.io import loadmat
+import numpy as np
+import IPython.display
+import PIL.Image
+
+class_dict = ["background", "lane_solid_white", "lane_broken_white", 
+			  "lane_double_white", "lane_solid_yellow", "lane_broken_yellow", 
+			  "lane_double_yellow", "lane_broken_blue", "lane_slow", 
+			  "stop_line", "arrow_left", "arrow_right", "arrow_go_straight", 
+			  "arrow_u_turn", "speed_bump", "crossWalk", "safety_zone", 
+			  "other_road_markings"]
+
+
+def getImageFromMat(mat):
+	mat_file = loadmat(mat)
+	rgb_seg_vp_label = mat_file['rgb_seg_vp']
+	img = rgb_seg_vp_label[:, :, :3]
+	return img
+
+def genColor(classes):
+	color_mat = [np.full((480, 640, 3),np.array([0, 0, 0]))]*classes
+	color_map = [np.full((1,3), np.array([0,0,0]))]*classes
+	for i in range(0, classes):
+			temp_colour = np.random.choice(range(256), size=3)
+			color_mat[i] = (np.full((480, 640, 3), temp_colour))
+			color_map[i] = (np.full((1, 3), temp_colour))
+	return color_mat, color_map
+
+def genColorSample(color):
+	example_mat = np.full((10, 10, 3), color)
+	return example_mat.astype(np.uint8)
+			
+def genLabeledImage(row_image, pixel_labels, color_mat, classes = 18):
+	# Pixel_labels have to be 480*640*1 rather than 480*640
+	labels = pixel_labels # WARNING: only designed for 480*640 for optimization for running
+	labels = np.concatenate((labels, labels, labels), axis = 2).astype(np.uint8)
+	temp_im = row_image
+	for i in range(1, classes): # from class 1 to max class #ZICHEN: to see the groundtruth, change to range(0, classes)
+		temp_im = np.where(labels == np.array([i, i, i]), color_mat[i], temp_im)
+	return temp_im.astype(np.uint8)
+	
+def getMaskFromMat(mat, classes = 18, color_mat = None):
+	alpha = 0.6 # set the transparency for mask
+	if color_mat == None:
+		# generate color matrix for different classes (TBC: can use fixed one.)
+		color_mat = [np.full((480, 640, 3),np.array([0, 0, 0]))]*classes
+		for i in range(0, classes):
+				temp_colour = np.random.choice(range(256), size=3)
+				color_mat[i] = (np.full((480, 640, 3), temp_colour))
+	mat_file = loadmat(mat)
+	rgb_seg_vp_label = mat_file['rgb_seg_vp']
+	img = rgb_seg_vp_label[:, :, :3]
+	seg = rgb_seg_vp_label[:, :, 3].reshape((480,640,1))
+	mixed_im = genLabeledImage(img, seg, color_mat, classes)
+	mixed_im = cv2.addWeighted(mixed_im, alpha, img, 1 - alpha, 0)
+	return mixed_im
+	
+def getHighestPath(search_path):
+	all_mat = list(Path(search_path).rglob("*.mat"))
+	h_value = [0]*18
+	h_path = [""]*18
+	for i in tqdm(all_mat):
+		mat_file = loadmat(i)
+		rgb_seg_vp_label = mat_file['rgb_seg_vp']
+		seg_label = rgb_seg_vp_label[:, :, 3]
+		for class_num in range(0, 18):
+			count = np.count_nonzero(seg_label == class_num)
+			if count >= h_value[class_num]:
+				h_value[class_num] = count
+				h_path[class_num] = i
+	return h_path
+
+
+if __name__ == "__main__":
+	# Recommend: below code run in Jupyter Notebook
+	classes = 18
+	root_path = "./"
+	h_path = getHighestPath(root_path)
+	color_mat, color_map = genColor(classes)
+	for i in range(1, classes):
+		print(class_dict[i], h_path[i])
+		color_ex = PIL.Image.fromarray(genColorSample(color_map[i]))
+		disp_im = PIL.Image.fromarray(getMaskFromMat(h_path[i], classes, color_mat))
+		IPython.display.display(color_ex)
+		IPython.display.display(disp_im)
---
+++ Appendix/vpgnet_pytorch.py
@@ -0,0 +1,103 @@
+# .mat file based dataloader
+# @ZICHEN
+#
+# -*- coding: utf-8 -*-
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+from scipy.io import loadmat
+
+# Dataset Class for loading VPG .mat database
+class VPG_dataset(Dataset):
+	def __init__(self,
+				 data_path,
+				 class_list,
+				 mode = 'train'):
+
+		assert os.path.exists(data_path), f"Error: {data_path} does not exists!"
+
+		self.mode = mode
+		self.class_list, self.num_class = self.getClassdict(class_list)
+		self.data_path = data_path
+		self.data_list, self.num_imgs = self.getDataList(data_path)
+		print(f"Mode: {self.mode}, Total data: {self.num_imgs}")
+
+	def getClassdict(self, class_list):
+		with open(class_list, 'r') as f:
+			classes = f.readlines()
+			classes = {i+1: classes[i].strip('\n') for i in range(len(classes))}
+		return classes, len(classes)
+
+	def getDataList(self, data_path):
+		with open(data_path, 'r') as f:
+			data_list = sorted(f.readlines())
+		return data_list, len(data_list)
+
+	def genImg(self, mat):
+		img = mat[:, :, :3] # RGB image
+		img = torch.from_numpy(img)
+		return img
+
+	def genVPMap(self, vp_mat):
+		x, y = np.where(vp_mat == 1)
+		map_stack = np.zeros((5, 480, 640))
+		x = int(x)
+		y = int(y)
+		map_stack[0, x, y] = 1
+		map_stack[1, :x+1, y:] = 1
+		map_stack[2, :x+1, :y+1] = 1
+		map_stack[3, x:, :y+1] = 1
+		map_stack[4, x:, y:] = 1
+		map_stack = torch.from_numpy(map_stack)
+		return map_stack
+
+	def genLabels(self, mat):
+		# Initialize the grid box matrix
+		grid_box = torch.LongTensor(4, 480, 640).fill_(0.)
+
+		# Read mat file into matrix
+		mat_data = loadmat(mat)
+		multi_label[0, :, :] = torch.from_numpy(mat_data[:, :, 3]) # TBC, for type matching
+		object_mask[0, :, :] = torch.from_numpy((mat_data[:, :, 3] != 0).astype(float))
+		vp = genVPMap(mat_data[:, :, 4])
+
+		multi_label = tf.Resize((60, 80), Image.NEAREST)(multi_label)
+		object_mask = tf.Resize((120, 160), Image.NEAREST)(object_mask)
+		vp = tf.Resize((120, 160), Image.NEAREST)(vp)
+		return [multi_label, object_mask, grid_box, vp]
+
+
+	def __getitem__(self, i):
+		mat = self.data_list[i]
+		img = self.genImg(mat)
+		multi_label, object_mask, grid_box, vp = self.genLabels(mat)
+		return {'image': img,
+				'multi_label': multi_label,
+				'object_mask': object_mask,
+				'grid_box': grid_box,
+				'vp': vp}
+
+
+# Main function
+if __name__=='__main__':
+
+	# set path to all the .mat data
+	# .mat database saves [r,g,b,class,vp] five channel info
+	data_path = '../t_data/train_list.txt'
+	class_list = '../data/classlist.txt'
+
+	vpg_dataset = VPG_dataset(data_path,
+							  class_list,
+							  mode = 'test')
+	
+	dataloader = DataLoader(vpg_dataset, 
+							batch_size = 1, 
+							shuffle = True)
+	
+	for i, batch in enumerate(dataloader):
+		print(f"image:
+		    {batch['image'].shape}, 
+		    multiLabel: {batch['multiLabel'].shape}, 
+		    objectMask: {batch['objectMask'].shape}")
+		op = np.array(batch['multiLabel'][0][12:15].mul(255.).clamp(0,255)).transpose(1,2,0)
